d8199593dbf14c3fae7513aa78c7fcd9544fc7d9
mauricio.denardi <mdenardi32@gmail.com>
2023-09-04 10:46:22 -0300

    JWT Fix

--- modules/login-register/login.controller.js
+++ modules/login-register/login.controller.js
@@ -22,7 +22,7 @@ export async function login (req,res){
 
     res.cookie("jwt",token,{
         httpOnly: true,
-        sameSite:None,
+        sameSite: "None",
         secure:true,
         maxAge: 24*60*60*1000
     })
